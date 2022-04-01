ff32ec787669f08873ad2cbbd1d564244d192698
leonely1990 <leonel.wow.1990@gmail.com>
2022-04-01 18:48:14 -0400

    Cambios en el js del boton "Crear Equipo"

--- proyecto02/static/js/all.js
+++ proyecto02/static/js/all.js
@@ -1,6 +1,14 @@
 let CrearEquipo = document.getElementById('botonCrear');
+let BotonCerrar = document.getElementById('botonCerrar');
 
 function mostrar() {
-    document.getElementById('formulario').style.display = 'contents';
+    document.getElementById('formulario').style.display = 'flex';
+    document.getElementsByName('body').style.BackgroudColor = "#d3e9fc00";
 }
 
+function cerrar(){
+    document.getElementById('formulario').style.display = 'none';
+}
+
+CrearEquipo.addEventListener('click', mostrar);
+BotonCerrar.addEventListener('click', cerrar);
